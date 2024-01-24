1374b159a2dde34bec4e23eb13d210f0569bb58c
eregha thompson <thompsoneregha005@gmail.com>
2024-01-24 06:09:40 -0800

    first commit

--- index.js
+++ index.js
@@ -12,15 +12,63 @@
 //}
 //)});
 
-function changeMode()
-{
-   let body = document.querySelector ('body');
-   let link = document.querySelector ('.link');
-    if (link.style.color== '#fff') {
-        body.style.background = '#fff'
-        link.style.color = '#191919'
-    } else {
-        body.style.background = '#191919'
-        link.style.color = '#fff';
-    }
+
+
+var body = document.querySelector('body');
+var btn = document.querySelector('.bttn');
+var img = document.querySelector('.home-img');
+var about = document.querySelector('.about-info');
+var nav = document.querySelector('.navbar');
+var links = document.querySelectorAll('.link');
+var text = document.querySelector('.text');
+var icons = document.querySelectorAll('.icon');
+var version = document.querySelector('.version');
+var icon_8 = document.querySelector('.icons-8');
+
+function darkmode() {
+    btn.innerHTML = 'whitemode';
+    body.style.background = 'black';
+    body.style.color = 'white';
+    btn.style.color = 'white';
+    img.style.opacity = '0.2';
+    about.style.opacity = '0.6';
+    nav.style.background = 'black';
+    links.forEach( link=> {
+        link.style.opacity = '0.5';
+      link.style.color = 'white';
+    }); 
+    text.style.opacity = '0.6';
+    icons.forEach(icon => {
+        icon.style.color = 'white';
+    });
+    version.style.opacity = '0.6';
+    icon_8.style.fill = 'white';
 }
+
+function lightmode() {
+    body.style.background = 'white';
+    body.style.color = 'black';
+    btn.style.color = 'black';
+    img.style.opacity = '1';
+    about.style.opacity = '1';
+    nav.style.background = 'white';
+    links.forEach( link=> {
+        link.style.opacity = '1';
+      link.style.color = 'black';
+    }); 
+    text.style.opacity = '1';
+    btn.innerHTML = 'darkmode';
+    icons.forEach(icon => {
+        icon.style.color = 'black';
+    });
+    version.style.opacity = '1';
+    icon_8.style.fill = '';
+}
+
+function changeMode(){
+    if (btn.innerHTML === 'whitemode') {
+        lightmode()
+    } else {
+        darkmode()
+    };
+};
